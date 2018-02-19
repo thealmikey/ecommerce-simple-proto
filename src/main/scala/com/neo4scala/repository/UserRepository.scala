57d07846f280e8f5478ac0fa7e103ad631b11a8e
TheAlmikey <thealmikey@gmail.com>
2018-02-19 15:12:46 +0300

    added UserRepository and empty Services

--- src/main/scala/com/neo4scala/repository/UserRepository.scala
+++ src/main/scala/com/neo4scala/repository/UserRepository.scala
@@ -1,5 +1,12 @@
 package com.neo4scala.repository
 
-class UserRepository {
+import java.util.UUID
 
+import com.neo4scala.model.User
+
+import scala.util.Try
+
+trait UserRepository {
+  def findUser(id: UUID): Try[Option[User]]
+  def updateUser(user: User): Try[Option[User]]
 }
